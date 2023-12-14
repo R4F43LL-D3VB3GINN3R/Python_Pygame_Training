8bae459da92bbd025edc46f150bbe5449a8bed54
Rafael Albuquerque <139004366+R4F43LL-D3VB3GINN3R@users.noreply.github.com>
2023-12-14 23:49:36 +0000

    Update level.py

--- level.py
+++ level.py
@@ -11,34 +11,35 @@ class Level(pygame.sprite.Sprite):
 #-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------#
 # [OBJECTS] #
     
-    def __init__(self):
+    def __init__(self, surface):
         super().__init__()
-        self.tile = Tile(600, 400, 50, 50, (0, 0, 255), 0)
-        self.map = stage_001
-        self.rect_size = self.tile.height 
+
+        self.display = surface
+        self.myrect = Tile(600, 400, 50, 50, (0, 0, 255), 0, self.display)
+        self.map = stage_001 * tile_size 
+        self.surface = surface 
+        self.rect_size = tile_size 
+
 
 #-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------#
 # [METHOD: 001] #
 
-    def printrect(self, surface):
+    def printrect(self):
 
-         for row_index, row in enumerate(self.map):
+        for row_index, row in enumerate(self.map):
             for col_index, cell in enumerate(row):
                 x = col_index * self.rect_size
                 y = row_index * self.rect_size
 
                 if cell == 'X':
-                    pygame.draw.rect(surface, (0, 0, 255), pygame.Rect(x, y, self.rect_size, self.rect_size))
+                    pygame.draw.rect(self.surface, (0, 0, 255), pygame.Rect(x, y, 50, 50))
 
 #-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------#
 # [METHOD: 002] #
 
-#-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------#
-# [METHOD: 004] #
+    def run(self):
 
-    def run(self, surface):
-
-        self.tile.run(surface)
-        self.printrect(surface)
+        self.myrect.update()
+        self.printrect()
 
 #-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------#
